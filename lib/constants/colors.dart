65d22097f7216d6d844875432f902ba71282e03c
Sviatoslav Leskiv <sviatoslav.leskiv@beleven.dev>
2022-06-26 12:27:54 +0300

    TextField maxLength changed

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class AppColors {
   const AppColors._();
 
-  static const Color background = Color(0xffC0028B);
+  static const Color purple = Color(0xffC0028B);
 
   static const Color white = Color(0xFFFFFFFF);
+
+  static const Color grey = Color(0xffE5E5E5);
 
   static const Color buttonColor = Color(0xffDB7CC1);
 }
--- lib/presentation/to_whom.dart
+++ lib/presentation/to_whom.dart
@@ -22,98 +22,98 @@ class _ToWhomPageState extends State<ToWhomPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.background,
+      backgroundColor: AppColors.purple,
       body: Padding(
         padding: EdgeInsets.only(
           top: MediaQuery.of(context).padding.top,
           bottom: doubleLightSpace * 2,
         ),
-        child: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: largeSpace),
-                child: Stack(
-                  children: [
-                    const Center(
-                      child: Text(
-                        'MoneyApp',
-                        style: TextStyle(
-                          fontSize: 16,
-                          height: 1.38,
-                          fontWeight: FontWeight.w600,
-                          color: AppColors.white,
-                        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: largeSpace),
+              child: Stack(
+                children: [
+                  const Center(
+                    child: Text(
+                      'MoneyApp',
+                      style: TextStyle(
+                        fontSize: 16,
+                        height: 1.38,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.white,
                       ),
                     ),
-                    Positioned(
-                      right: largeSpace,
-                      child: IconButton(
-                        constraints: const BoxConstraints(),
-                        padding: EdgeInsets.zero,
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.cancel,
-                          color: Colors.white,
-                        ),
+                  ),
+                  Positioned(
+                    right: largeSpace,
+                    child: IconButton(
+                      constraints: const BoxConstraints(),
+                      padding: EdgeInsets.zero,
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.cancel,
+                        color: Colors.white,
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              const SizedBox(height: mediumSpace * 9),
-              const Text(
-                "To whom?",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.w600,
-                  color: AppColors.white,
-                ),
+            ),
+            const SizedBox(height: mediumSpace * 9),
+            const Text(
+              "To whom?",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
+                color: AppColors.white,
               ),
-              const SizedBox(height: doubleMediumSpace * 5),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: doubleLargeSpace * 2),
-                child: SizedBox(
-                  height: doubleMediumSpace * 2,
-                  child: TextField(
-                    controller: myController,
-                    textAlign: TextAlign.center,
-                    cursorColor: AppColors.white,
-                    style: const TextStyle(
-                      color: AppColors.white,
-                      fontWeight: FontWeight.w500,
+            ),
+            const SizedBox(height: doubleMediumSpace * 5),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: doubleLargeSpace * 2),
+              child: SizedBox(
+                height: doubleMediumSpace * 2,
+                child: TextField(
+                  maxLength: 25,
+                  controller: myController,
+                  textAlign: TextAlign.center,
+                  cursorColor: AppColors.white,
+                  style: const TextStyle(
+                    color: AppColors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  decoration: const InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: AppColors.white,
+                        width: lightSpace / 4,
+                      ),
                     ),
-                    decoration: const InputDecoration(
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: AppColors.white,
-                          width: lightSpace / 4,
-                        ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: AppColors.white,
+                        width: lightSpace / 4,
                       ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: AppColors.white,
-                          width: lightSpace / 4,
-                        ),
-                      ),
-                      border: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: AppColors.white,
-                          width: lightSpace / 4,
-                        ),
+                    ),
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: AppColors.white,
+                        width: lightSpace / 4,
                       ),
                     ),
                   ),
                 ),
               ),
-              const Spacer(),
-              AppButton.mainButton(
-                title: "Pay",
-                onPressed: () {},
-              ),
-            ],
-          ),
+            ),
+            const Spacer(),
+            AppButton.mainButton(
+              title: "Pay",
+              onPressed: () {},
+            ),
+          ],
         ),
       ),
     );
